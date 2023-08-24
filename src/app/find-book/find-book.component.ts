58325f68806dc14768923ad34c429234dbe78d16
Vitoriono <korovin.vic@gmail.com>
2023-08-24 22:28:35 +0300

    feat:[API]Complete find-book page

--- src/app/find-book/find-book.component.ts
+++ src/app/find-book/find-book.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../services/api.service';
+import { IBook } from '../interfaces';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs';
 
 @Component({
   selector: 'app-find-book',
@@ -6,5 +10,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./find-book.component.scss']
 })
 export class FindBookComponent {
+  declare books: Array<IBook>
+  declare findForm: FormControl;
+  declare invalid: string;
+
+  constructor(private apiServ: ApiService){}
+
+  getInputs(){
+    this.findForm = new FormControl();
+    this.findForm.valueChanges.pipe(map(value => {
+      return value}),
+      debounceTime(800),
+      distinctUntilChanged())
+      .subscribe(value => {
+        this.getFindBook(value);
+      })
+  }
+
+  getFindBook(findBook: string){
+    this.apiServ.getBooks().subscribe(data => {this.books = data
+      .filter(value => value.title.includes(findBook))
+      if(this.books.length === 0){
+        this.invalid = 'Немає такої книги в списку';
+      }else {
+        this.invalid = '';
+      }
+  })
+}
+
+getAllBooks(){
+  this.apiServ.getBooks().subscribe(data => this.books = data);
+}
+
+  ngOnInit(){
+    this.getInputs();
+    this.getAllBooks();
+  }
 
 }
